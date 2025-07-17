3197316440842903cf6cb2984f6c8c257c2cc4d6
Banji-Atoyebi <atoyebiolabanjib@gmail.com>
2025-07-17 11:50:06 +0100

    applied inline css styling to react components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#f0f8ff', fontStyle: 'italic' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
