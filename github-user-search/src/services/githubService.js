5e608aa4ca5ad99ca7b233b6b634723300aa7585
Banji-Atoyebi <atoyebiolabanjib@gmail.com>
2025-08-01 13:37:31 +0100

    githubService.js updated

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,16 +1,25 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com/users";
-const token = import.meta.env.VITE_APP_GITHUB_TOKEN; 
+const SEARCH_URL = "https://api.github.com/search/users";
+const token = import.meta.env.VITE_APP_GITHUB_TOKEN;
 
-export const fetchUserData = async (username) => {
+export const advancedSearchUsers = async ({ username, location, minRepos }) => {
   try {
-    const res = await axios.get(`${BASE_URL}/${username}`, {
+    let queryParts = [];
+
+    if (username) queryParts.push(`${username} in:login`);
+    if (location) queryParts.push(`location:${location}`);
+    if (minRepos) queryParts.push(`repos:>=${minRepos}`);
+
+    const query = queryParts.join(" ");
+
+    const res = await axios.get(`${SEARCH_URL}?q=${encodeURIComponent(query)}`, {
       headers: token ? { Authorization: `token ${token}` } : {},
     });
+
     return res.data;
   } catch (error) {
-    console.error("Error fetching GitHub user:", error);
+    console.error("Error fetching advanced search:", error);
     return null;
   }
 };
