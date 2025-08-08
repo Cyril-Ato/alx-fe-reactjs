271187328b5a0447b082fac37be3ac1e81575f49
Banji-Atoyebi <atoyebiolabanjib@gmail.com>
2025-08-08 11:40:56 +0100

    UserProfile.jsx update latest

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,9 +1,6 @@
-// Required Tailwind classes for checker
-const dummy = ["sm:p-4", "md:p-8"];
-
 function UserProfile() {
   return (
-    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
+    <div className="bg-gray-100 md:p-8 sm:p-4 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
