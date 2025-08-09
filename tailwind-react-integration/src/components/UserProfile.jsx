549d1048046a261d7df7ef6f073d16293f289c17
Banji-Atoyebi <atoyebiolabanjib@gmail.com>
2025-08-09 10:09:41 +0100

    final update

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -7,10 +7,10 @@ function UserProfile() {
         className="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
       />
       <h1 className="text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500 transition-colors duration-300">
-        John Doe
+        Cyril Ato
       </h1>
       <p className="text-sm md:text-base text-gray-600">
-        Developer at Example Co. Loves to write code and explore new technologies.
+        Developer at OTC Co. Loves to write code and explore new technologies.
       </p>
     </div>
   );
