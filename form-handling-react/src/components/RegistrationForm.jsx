8853eb12f44214f6556b305b0fa0ea206fa89d81
Banji-Atoyebi <atoyebiolabanjib@gmail.com>
2025-08-19 14:18:26 +0100

    RegistrationForm.jsx updated

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,25 +1,13 @@
 import { useState } from "react";
 
 export default function RegistrationForm() {
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
-
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const { username, email, password } = formData;
 
     
     if (!username || !email || !password) {
@@ -28,6 +16,8 @@ export default function RegistrationForm() {
     }
 
     setError("");
+
+    const formData = { username, email, password };
 
     
     try {
@@ -58,8 +48,8 @@ export default function RegistrationForm() {
         <input
           type="text"
           name="username"
-          value={formData.username}
-          onChange={handleChange}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
           className="border p-2 w-full"
         />
       </div>
@@ -69,8 +59,8 @@ export default function RegistrationForm() {
         <input
           type="email"
           name="email"
-          value={formData.email}
-          onChange={handleChange}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           className="border p-2 w-full"
         />
       </div>
@@ -80,8 +70,8 @@ export default function RegistrationForm() {
         <input
           type="password"
           name="password"
-          value={formData.password}
-          onChange={handleChange}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           className="border p-2 w-full"
         />
       </div>
