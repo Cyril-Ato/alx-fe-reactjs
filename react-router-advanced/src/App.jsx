34e9bb38fe78da4a71309e1c278249887581355b
Banji-Atoyebi <atoyebiolabanjib@gmail.com>
2025-08-20 14:36:18 +0100

    <BrowsreRouter> included in App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import BlogPost from "./pages/BlogPost";
@@ -8,34 +8,36 @@ import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
-    <div className="p-6">
-      <nav className="space-x-4 mb-6">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/posts/1">Sample Blog Post</Link>
-      </nav>
+    <BrowserRouter>
+      <div className="p-6">
+        <nav className="space-x-4 mb-6">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/profile">Profile</Link>
+          <Link to="/posts/1">Sample Blog Post</Link>
+        </nav>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
 
-        
-        <Route path="/posts/:id" element={<BlogPost />} />
+          
+          <Route path="/posts/:id" element={<BlogPost />} />
 
-        
-        <Route
-          path="/profile/*"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
+          
+          <Route
+            path="/profile/*"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
 
-        
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
+          
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
