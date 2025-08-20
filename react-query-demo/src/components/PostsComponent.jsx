7e8d041c773afcfd7d96db6e33bf655ee4068fe5
Banji-Atoyebi <atoyebiolabanjib@gmail.com>
2025-08-20 13:01:27 +0100

    edit to app.jsx and postcomponents.jsx

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery } from "@tanstack/react-query";
 
 const fetchPosts = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
